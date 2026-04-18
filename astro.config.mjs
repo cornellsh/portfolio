b54f82c2f717ff54bdf21850cfef69eb7af4d746
cornellsh <kornelshakh@proton.me>
2026-04-18 21:58:17 +0200

    Astro: enable prefetch, compressHTML, remove unused deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,12 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport',
+  },
+  compressHTML: true,
+  build: {
+    inlineStylesheets: 'auto',
+  },
 });
